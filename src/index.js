1c16a9cd1b6a747c3e72018932d5fea403a55a5c
dkundel <hi@dominik.dev>
2019-12-10 11:27:35 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { resolve } = require('path');
+const { info } = require('log-symbols');
+
+const dataRouter = require('./routes/data');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/data', dataRouter);
+app.use(express.static(resolve(__dirname, '../static')));
+
+app.listen(PORT, () => {
+  console.log(`%s Server listening on port %d`, info, PORT);
+});
